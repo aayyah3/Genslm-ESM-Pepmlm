b970b179066edf0ceb3ed889d7183e4986d883c4
braceal <braceal@umich.edu>
2023-08-30 19:35:47 -0700

    conflict

--- weight_watcher.py
+++ weight_watcher.py
@@ -8,10 +8,10 @@ import pandas as pd
 def main(model_path: Path, output_path: Path) -> None:
     model = EsmForMaskedLM.from_pretrained(model_path)
     watcher = ww.WeightWatcher(model=model)
-    details = watcher.analyze(plot=True, savefig=str(output_path), randomize=True)
+    details = watcher.analyze(plot=False, savefig=str(output_path), randomize=True)
     summary = watcher.get_summary()
     details.to_csv(str(output_path / "details.csv"))
-    pd.DataFrame(summary).to_csv(str(output_path / "summary.csv"))
+    pd.DataFrame(summary, index=[0]).to_csv(str(output_path / "summary.csv"))
 
 
 if __name__ == "__main__":
