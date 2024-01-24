290ce2ccec541c5dafa75b12a0f6f82f42a13c25
braceal <abrace05@gmail.com>
2024-01-24 00:04:12 -0600

    pbs

---
+++ examples/ec/polaris-pbs-v2.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -l
+#PBS -l select=1:system=polaris
+#PBS -l place=scatter
+#PBS -l filesystems=home:eagle
+#PBS -l walltime=0:60:00
+#PBS -q debug
+#PBS -A FoundEpidem
+#PBS -o $PBS_O_WORKDIR/$PBS_JOBID.out
+#PBS -e $PBS_O_WORKDIR/$PBS_JOBID.err
+
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS=1 # Number of MPI ranks to spawn per node
+NDEPTH=64 # Number of hardware threads per rank (i.e. spacing between MPI ranks)
+GPU_PER_NODE=4
+NTOTRANKS=$(( NNODES * NRANKS ))
+
+export http_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
+export https_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
+export ftp_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
+
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS}"
+
+# Change to work directory to write hostfile and .deepspeed_env file to a unique location
+cd $PBS_O_WORKDIR
+
+# Setup the deepspeed hostfile
+cat $PBS_NODEFILE > hostfile 
+sed -e 's/$/ slots=4/' -i hostfile
+
+master_node=$(cat $PBS_NODEFILE| head -1)
+export MASTER_ADDR=$(host $master_node | head -1 | awk '{print $4}')
+
+# Load modules
+module load conda/2023-10-04
+conda activate evoforecast
+
+# Set environment variables
+export HF_HOME=/lus/eagle/projects/CVD-Mol-AI/braceal/cache/huggingface
+
+# The path to the run files
+accelerate_config_file=/lus/eagle/projects/CVD-Mol-AI/braceal/src/genslm-esm/examples/ec/deepspeed_configs/deepspeed_ddp_dynamo_single_node.yaml
+python_script=/lus/eagle/projects/CVD-Mol-AI/braceal/src/genslm-esm/genslm_esm/train.py
+
+# Check if a config file is provided as a command-line argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <config_file>"
+    exit 1
+fi
+
+config_file="$1"
+
+# launch the job using accelerate, which will spawn the MPI processes (no need for mpiexec)
+accelerate launch \
+--config_file ${accelerate_config_file} \
+ --main_process_ip ${MASTER_ADDR} \
+ --main_process_port 25900 \
+ --num_machines ${NNODES} \
+ --num_processes $((NTOTRANKS * GPU_PER_NODE)) \
+ --deepspeed_hostfile hostfile \
+ ${python_script} --config ${config_file}
